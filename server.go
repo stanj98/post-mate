49f2892f7645cd19d7759801c74b2f832b40db63
Stanley John <stanleyjohn98@gmail.com>
2023-12-18 19:22:51 +0530

    added comment on new features

--- server.go
+++ server.go
@@ -142,9 +142,9 @@ func main() {
 
 	/*
 		Plan:
-		1. Create note structure - perform CRUD and duplicate notes
-		2. Use mongodb instead of point 1
-		3. Refer to this: https://dev.to/percoguru/getting-started-with-apis-in-golang-feat-fiber-and-gorm-2n34
+		2. Set up templates to communicate with created APIs
+		3. Use mongodb instead of memory data in this file
+		4. Refer to this: https://dev.to/percoguru/getting-started-with-apis-in-golang-feat-fiber-and-gorm-2n34
 
 	*/
 

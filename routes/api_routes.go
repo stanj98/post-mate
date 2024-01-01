74eb0c17da3c1bd192b296174cb5198ea3b7158f
Stanley John <stanleyjohn98@gmail.com>
2024-01-01 14:30:02 +0530

    fixing up new errors after package creation

---
+++ routes/api_routes.go
@@ -0,0 +1,92 @@
+package api
+
+import (
+	"net/http"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/stanj98/post-mate/types"
+	"github.com/stanj98/post-mate/util"
+)
+
+var notes = []*types.Note
+
+// var notes = []*types.Note {
+// 	{
+// 		Id: "1",
+// 		Title: "First Note",
+// 		ContentBody: "My first note",
+// 		ContentImgURL: "https://png.pngtree.com/element_pic/12/03/20/1656e3fa305853d.jpg", 
+// 		CreatedDate: time.Now(),
+// 	},
+// 	{
+// 		Id: "3",
+// 		Title: "Third Note",
+// 		ContentBody: "My boring note", 
+// 		CreatedDate: time.Now(),
+// 	},
+// } 
+
+func GetNotes() ([]*types.Note) {
+	return notes
+}
+
+func getNotes(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"data" : notes})
+} 
+
+func getNote(c *gin.Context) {
+	id := c.Param("id")
+	note, err := *util.GetNoteById(notes, id)
+	
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message" : "Unable to find note!"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{"data" : note})
+}
+
+func createNote(c *gin.Context) {
+	var newNote *types.Note
+	if err := c.BindJSON(&newNote); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message" : err})
+		return
+	}
+	*util.SetData(&newNote)
+	notes = append(notes, newNote)
+	c.IndentedJSON(http.StatusCreated, gin.H{"data" : newNote})
+}
+
+func editNote(c *gin.Context) {
+	var oldNote *types.Note
+	id := c.Param("id")
+	if err := c.BindJSON(&oldNote); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message" : "Error parsing JSON data"})
+		return
+	}
+	note, err := *util.EditNoteById(notes, id, &oldNote)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message" : "Error! Unable to edit note"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, note)
+}
+
+func deleteNote(c *gin.Context) {
+	id := c.Param("id")
+	note, err := *util.GetNoteById(notes, id)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Error! Unable to find note"})
+		return
+	}
+	notes, err := *util.DeleteNote(notes, note)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message" : "Error! Unable to delete note"})
+		return
+	}
+	data := gin.H{
+		"notes": notes,
+	}
+	c.IndentedJSON(http.StatusOK, data)
+}
---
+++ routes/view_routes.go
@@ -0,0 +1,68 @@
+package api
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/stanj98/post-mate/util"
+)
+
+func CreateNote(c *gin.Context) {
+	//try getting data inputted from user when going from edit note -> create note button click and show in create note page
+	//like how quicknote does
+	c.HTML(http.StatusOK, "create-note.html", nil)
+}
+
+func CloneNote(c *gin.Context) {
+	id := c.Param("id")
+	notes := GetNotes()
+	note, err := *util.GetNoteById(notes, id)
+	if err != nil {
+		c.HTML(http.StatusOK, "view-note.html", nil)
+		return
+	}
+	data := gin.H{
+		"note": note,
+	}
+	c.HTML(http.StatusOK, "clone-note.html", data)
+}
+
+
+func EditNote(c *gin.Context) {
+	id := c.Param("id")
+	notes := GetNotes()
+	note, err := *util.GetNoteById(notes, id)
+
+	if err != nil {
+		c.HTML(http.StatusOK, "view-note.html", nil)
+		return
+	}
+	
+	data := gin.H{
+		"note": note,
+	}
+	c.HTML(http.StatusOK, "edit-note.html", data)
+}
+
+func ViewNote(c *gin.Context) {
+	id := c.Param("id")
+	notes := GetNotes()
+	note, err := *util.GetNoteById(notes, id)
+	if err != nil {
+		c.HTML(http.StatusOK, "create-note.html", nil)
+		return
+	}
+	
+	data := gin.H{
+		"note": note,
+	}
+	c.HTML(http.StatusOK, "view-note.html", data)
+}
+
+func ViewNotes(c *gin.Context) {
+	data := gin.H{
+		"notes" : notes,
+	}
+	c.HTML(http.StatusOK, "view-notes.html", data)
+}
+
+
